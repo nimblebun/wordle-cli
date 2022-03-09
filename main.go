e00f1c16be54f6643a0b433faf21f23f94ee8dcb
József Sallai <jozsef@sallai.me>
2022-03-09 11:01:52 +0200

    bump version number

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ func main() {
 		Name:    "wordle-cli",
 		Usage:   "play wordle in your terminal",
 		Action:  startOfficial,
-		Version: "1.0.5",
+		Version: "1.0.6",
 		Commands: []*cli.Command{
 			{
 				Name:   "official",
