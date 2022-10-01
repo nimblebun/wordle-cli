f077c45889dca9e8be40c106f9560be9d082e6af
Erin Heimer <heimer16@protonmail.com>
2022-10-01 11:27:05 -0600

    Use local timezone for word of day detection

--- words/words.go
+++ words/words.go
@@ -13,22 +13,22 @@ import (
 )
 
 func getWordOfTheDay(epoch time.Time) (string, int) {
-	now := time.Now().UTC()
+	now := time.Now()
 	year, month, day := now.Year(), now.Month(), now.Day()
 
-	currentDay := time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
+	currentDay := time.Date(year, month, day, 0, 0, 0, 0, time.Local)
 
 	idx := int(math.Round(currentDay.Sub(epoch).Hours()/24)) % len(WordList)
 	return WordList[idx], idx
 }
 
 func GetOfficialWordOfTheDay() (string, int) {
-	wordleDay := time.Date(2021, time.Month(6), 19, 0, 0, 0, 0, time.UTC)
+	wordleDay := time.Date(2021, time.Month(6), 19, 0, 0, 0, 0, time.Local)
 	return getWordOfTheDay(wordleDay)
 }
 
 func GetWordOfTheDay() (string, int) {
-	wordleCliDay := time.Date(2001, time.Month(3), 7, 0, 0, 0, 0, time.UTC)
+	wordleCliDay := time.Date(2001, time.Month(3), 7, 0, 0, 0, 0, time.Local)
 	return getWordOfTheDay(wordleCliDay)
 }
 
